e02f06d01fe6a19a7a172818922773c0b86369eb
codewithsmile-real <alokjha5705@gmail.com>
2025-04-06 10:47:43 +0000

    datatypes

---
+++ dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+
+//alert("hello") //we are using nodejs,not browser
+
+console.log(3+3)
+console.log();
+
+let name ="hitesh"
+let age=18
+let isLoggedIn=false
+
+//number => 2 to power 53
+//bigint
+//string=> ""
+//boolean=> true/false
+//null=>standalone value
+//undefined=>
+//symbol => unique
+
+
+// object 
+
+console.log(typeof "hitesh");
+console.log(typeof null);// object
